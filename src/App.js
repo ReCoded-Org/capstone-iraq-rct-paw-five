7a10dbeb06e410d00d64db5e50b70d532f8b71f9
zeena kareem <zena342699@gmail.com>
2021-03-17 13:24:19 +0300

    add firebase config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect } from 'react'
+import firebase from './db/firebase'
+ 
 function App() {
+
+const ref=firebase.firestore().collection("pets")
+
+useEffect(()=>{
+ref.onSnapshot((querySnapshot)=>{
+  querySnapshot.forEach((doc)=>{
+    console.log(doc.data())
+  })
+})
+})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     paw-five 
     </div>
   );
 }
-
+ 
 export default App;
