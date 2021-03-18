cabdb2d790a8472b230bd6fd1e36caf1d334b22b
Ali <65448118+Ali-mustafa8@users.noreply.github.com>
2021-03-18 14:07:51 +0300

    setting up react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
  
   return (
-    <>paw five</>
+    <div className="App">
+      <h1>PawFive</h1>
+    </div>
   );
 }
 
