b2734782a3fb39ec349e93bfcf5b791755713a2d
Ali <65448118+Ali-mustafa8@users.noreply.github.com>
2021-03-18 23:51:37 +0300

    update-react-routers

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
  
   return (
-    <div className="App">
-      <h1>PawFive</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">{/**<Home/> */}</Route>
+        <Route path="/AdoptionPage">{/**<AdoptionPage/> */}</Route>
+        <Route path="/ResourcePage">{/**<ResourcePage/> */}</Route>
+        <Route path="/Aboutus">{/**<Aboutus/> */}</Route>
+        <Route path="/DetailsPage">{/**<DetailsPage/> */}</Route>
+        <Route path="/ContactUs">{/**<ContactUs/> */}</Route>
+        <Route path="/Login">{/**<Login/> */}</Route>
+        <Route path="/Signup">{/**<Signup/> */}</Route>
+      </Switch>
+    </Router>
   );
 }
 
