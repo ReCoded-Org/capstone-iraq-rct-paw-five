c359d4052e630bd31e363e4b11e0dc86b84de3bc
Ali <65448118+Ali-mustafa8@users.noreply.github.com>
2021-03-20 23:20:46 +0300

    setting up react-router-dom (#16)
    * setting up react-router-dom
    
    
    Co-authored-by: Muhanned Noman <Muhanned.Noman@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
  
   return (
-    <>paw five</>
+    <Router>
+      <Switch>
+        <Route path="/">{/**<Home/> */}</Route>
+        <Route path="/AdoptionPage">{/**<AdoptionPage/> */}</Route>
+        <Route path="/ResourcePage">{/**<ResourcePage/> */}</Route>
+        <Route path="/Aboutus">{/**<Aboutus/> */}</Route>
+        <Route path="/DetailsPage">{/**<DetailsPage/> */}</Route>
+        <Route path="/ContactUs">{/**<ContactUs/> */}</Route>
+        <Route path="/Login">{/**<Login/> */}</Route>
+        <Route path="/Signup">{/**<Signup/> */}</Route>
+      </Switch>
+    </Router>
   );
 }
 
