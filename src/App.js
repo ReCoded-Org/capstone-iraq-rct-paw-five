323664e4330e35eb0878139c537e6b626a4b26b1
Muhanned Noman <Muhanned.Noman@gmail.com>
2021-03-20 23:19:39 +0300

    Fix: update yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
  
