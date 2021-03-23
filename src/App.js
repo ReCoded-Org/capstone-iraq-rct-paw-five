398395872b5a8331241ae7bb26e0e43bbcf34842
areeg94fahad <fahedareeg@gmail.com>
2021-03-23 09:58:06 -0700

    Fix NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NavBar from './components/NavBar/NavBar';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
+import {
+    HOME_ROUTE,
+    ADOPT_ROUTE,
+    ABOUT_ROUTE,
+    RESOURCE_CAT_ROUTE,
+    RESOURCE_DOG_ROUTE,
+    LOGIN_ROUTE,
+    SIGNUP_ROUTE,
+    CONTACT_US_ROUTE,
+} from './routes'
 
 function App() {
- 
-  return (
-    <Router>
-      <NavBar/>
-      <Switch>
-        <Route path="/" exact>{/** <Home/> */}</Route>
-        <Route path="/Adopt" exact>{/** <AdoptionPage/> */}</Route>
-        <Route path="/ResourcePage" exact>{/** <ResourcePage/> */}</Route>
-        <Route path="/areeg" exact>{/** <Aboutus/> */}</Route>
-        <Route path="/DetailsPage" exact>{/** <DetailsPage/> */}</Route>
-        <Route path="/ContactUs" exact>{/** <ContactUs/> */}</Route>
-        <Route path="/Login" exact>{/** <Login/> */}</Route>
-        <Route path="/Signup" exact>{/** <Signup/> */}</Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <NavBar />
+            <Switch>
+                <Route path={ADOPT_ROUTE}>{/** <ADOPT_ROUTE/> */}</Route>
+                <Route path={ABOUT_ROUTE}>{/** <ABOUT_ROUTE/> */}</Route>
+                <Route path={LOGIN_ROUTE}>{/** <LOGIN_ROUTE/> */}</Route>
+                <Route path={SIGNUP_ROUTE}>{/** <SIGNUP_ROUTE/> */}</Route>
+                <Route path={RESOURCE_CAT_ROUTE}>
+                    {/** <RESOURCE_CAT_ROUTE/> */}
+                </Route>
+                <Route path={RESOURCE_DOG_ROUTE}>
+                    {/** <RESOURCE_DOG_ROUTE/> */}
+                </Route>
+                <Route path={CONTACT_US_ROUTE}>
+                    {/** <CONTACT_US_ROUTE/> */}
+                </Route>
+                <Route path={HOME_ROUTE} exact>
+                    {/** <HOME_ROUTE/> */}
+                </Route>
+            </Switch>
+        </Router>
+    )
 }
 
-export default App;
+export default App
