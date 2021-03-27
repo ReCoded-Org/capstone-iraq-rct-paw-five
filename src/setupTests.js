7a4813b3965639de8e608d7763dcdfc33e07b1fa
Muhanned Noman <Muhanned.Noman@gmail.com>
2021-03-27 16:59:28 +0300

    Fix: test and update snapshot

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  fallbackLng: 'en',
+
+  ns: ['translations'],
+  defaultNS: 'translations',
+
+  resources: { en: { translations: {} } },
+});
