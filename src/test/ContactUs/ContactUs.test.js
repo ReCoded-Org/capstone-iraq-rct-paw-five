edfd505900c45a0cdecc80f38f321556379d3dcf
AREEG94FAHAD <fahedareeg@gmail.com>
2021-04-02 15:05:45 +0300

    Contact us v2 (#64)
    * Fix ContactUs

---
+++ src/test/ContactUs/ContactUs.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import ContactUs from '../../containers/ContactUs/ContactUs'
+import AllReducers from '../../redux/reducers/AllReducers'
+const store = createStore(AllReducers)
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <Router>
+          <ContactUs store={store} />
+        </Router>
+      </Provider>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
