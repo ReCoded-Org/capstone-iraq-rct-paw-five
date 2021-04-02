d992a72c5639021ce196200afe52bfb427bbbcca
areeg94fahad <fahedareeg@gmail.com>
2021-04-02 01:02:35 -0700

    Fix ContactUs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import AddPet from './components/AddPet/AddPet'
+import ContacUS from './containers/ContactUs/ContactUs'
 import {
   HOME_ROUTE,
   ADOPT_ROUTE,
@@ -30,7 +31,9 @@ function App() {
         <Route path={SIGNUP_ROUTE}>{/** <SIGNUP_ROUTE/> */}</Route>
         <Route path={RESOURCE_CAT_ROUTE}>{/** <RESOURCE_CAT_ROUTE/> */}</Route>
         <Route path={RESOURCE_DOG_ROUTE}>{/** <RESOURCE_DOG_ROUTE/> */}</Route>
-        <Route path={CONTACT_US_ROUTE}>{/** <CONTACT_US_ROUTE/> */}</Route>
+        <Route path={CONTACT_US_ROUTE} component={ContacUS}>
+          {/** <CONTACT_US_ROUTE/> */}
+        </Route>
         <Route path={HOME_ROUTE} exact>
           {/** <HOME_ROUTE/> */}
         </Route>
