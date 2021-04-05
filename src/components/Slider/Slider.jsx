efcdc896acd46d75725626dcfc42a3d41ccd1bfc
AREEG94FAHAD <fahedareeg@gmail.com>
2021-04-05 19:31:38 +0300

    Add Home page (#67)
    Feat: Add home page component

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Slide } from 'react-slideshow-image'
+import 'react-slideshow-image/dist/styles.css'
+
+export default function Slider({ pets }) {
+  return (
+    <div className="slide-container" dir="ltr">
+      {pets ? (
+        <Slide>
+          {pets.map(pet => (
+            <div
+              key={pet.id}
+              className="each-slide d-flex justify-content-center m-5"
+            >
+              <img
+                src={pet.file}
+                className="img-fluid"
+                alt=""
+                style={{
+                  borderRadius: '40px',
+                  height: '500px',
+                }}
+              />
+            </div>
+          ))}
+        </Slide>
+      ) : (
+        ''
+      )}
+    </div>
+  )
+}
+
+Slider.propTypes = {
+  pets: null,
+}
+
+Slider.defaultProps = {
+  pets: {},
+}
---
+++ src/test/Slider/Slider.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Slider from '../../components/Slider/Slider'
+
+describe('Slider', () => {
+  test('renders App component', () => {
+    render(<Slider pets={[]} />)
+  })
+})
