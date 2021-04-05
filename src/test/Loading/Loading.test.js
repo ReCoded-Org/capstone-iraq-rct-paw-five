93869b2572b394a62b9a11339ef647a9ea5789f6
Muhanned Noman <Muhanned.Noman@gmail.com>
2021-04-05 19:38:57 +0300

    Feat: add loading component

---
+++ src/test/Loading/Loading.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Loading from '../../components/Loading/Loading'
+import AllReducers from '../../redux/reducers/AllReducers'
+
+const store = createStore(AllReducers)
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <Router>
+          <Loading store={store} />
+        </Router>
+      </Provider>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
