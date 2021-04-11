42165c88f45b8a1b86453357ebcf8be7a92bf9e3
areeg94fahad <fahedareeg@gmail.com>
2021-04-11 04:18:40 -0700

    Add my pet page

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,3 +1,5 @@
+import firebase from '../../firebase'
+
 export function selectedLang(lang) {
   return {
     type: 'SELECTED_LANG',
@@ -5,11 +7,66 @@ export function selectedLang(lang) {
   }
 }
 
-//   to acept export statement and pass eslintrc
 
-export function text(lang) {
-  return {
-    type: 'SELECTED_LANG',
-    payload: lang,
-  }
+
+export function fetchMyPets (name,sortType,uid) {
+    return async (dispatch) => {
+        if(uid)
+        if (name === null ) {
+            firebase
+                .firestore()
+                .collection('pets')
+                .where('uid', '==',uid )
+                .orderBy(sortType)
+                .onSnapshot((shnapshot) => {
+                    const newPets = shnapshot.docs.map((doc) => ({
+                        id: doc.id,
+                        ...doc.data(),
+                    }))
+                    dispatch({
+                        type: 'FETCH_MY_PETS',
+                        payload: newPets,
+                    })
+                })
+        } else {
+            
+            firebase
+                .firestore()
+                .collection('pets')
+                .where('uid', '==',uid )
+                .orderBy(sortType)
+                .startAt(name)
+                .endAt(`${name}\uf8ff`)
+                .onSnapshot((shnapshot) => {
+                    const newPets = shnapshot.docs.map((doc) => ({
+                        id: doc.id,
+                        ...doc.data(),
+                    }))
+                    dispatch({
+                        type: 'FETCH_MY_PETS',
+                        payload: newPets,
+                    })
+                })
+        }
+   }
+}
+
+export function deleteMyPett(id) {
+    return (dispatch) => {
+        firebase.firestore().collection('pets').doc(id).delete()
+        dispatch({
+            type: 'DELETE_MY_PET',
+        })
+    }
+}
+
+export function UpdateMyPett(id, adopt) {
+    return (dispatch) => {
+        firebase.firestore().collection('pets').doc(id).update({
+            adopted: adopt,
+        })
+        dispatch({
+            type: 'UPDATE_MY_PET',
+        })
+    }
 }
--- src/redux/reducers/AllReducers.js
+++ src/redux/reducers/AllReducers.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux'
-import petReducer, { langReducer } from './petReducer'
+import petReducer, { langReducer,myPets, deleteMyPet, updateMyPet } from './petReducer'
 import user from './user'
 
 export default combineReducers({
   langReducer,
   pets: petReducer,
   user,
+  myPets,
+  deleteMyPet,
+  updateMyPet
+  
 })
